fd79e682a5fe35740661ee106163ea740b044c18
Jay Butera <buterajay@protonmail.com>
2021-05-05 17:29:04 -0500

    Add deps and fix collection name

--- info.rkt
+++ info.rkt
@@ -1,5 +1,6 @@
 #lang info
 
-(define collection 'mel-types)
+(define collection "mel-types")
 (define pkg-desc "a library for generating json-encoded mel types.")
 (define version "0.0.1")
+(define deps '("json"))
